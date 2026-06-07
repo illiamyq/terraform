3990ab8b24b63194373a7c40bf42467dc06975c6
Illia <freezeyourdiary@gmail.com>
2026-06-07 12:07:14 +0200

    -__- fix handler reload in handlers

--- functions/order_ingest/handler.py
+++ functions/order_ingest/handler.py
@@ -3,25 +3,22 @@ import boto3
 import os
 import uuid
 
-s3 = boto3.client('s3')
-sqs = boto3.client('sqs')
 
 def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    sqs = boto3.client('sqs')
     body = json.loads(event.get('body', '{}'))
     order_id = str(uuid.uuid4())
     body['order_id'] = order_id
-
     s3.put_object(
         Bucket=os.environ['ORDER_BUCKET'],
         Key=f"orders/{order_id}.json",
         Body=json.dumps(body)
     )
-
     sqs.send_message(
         QueueUrl=os.environ['ORDER_QUEUE_URL'],
         MessageBody=json.dumps(body)
     )
-
     return {
         'statusCode': 200,
         'body': json.dumps({'order_id': order_id})
--- functions/order_processor/handler.py
+++ functions/order_processor/handler.py
@@ -2,19 +2,16 @@ import json
 import boto3
 import os
 
-sns = boto3.client('sns')
 
 def lambda_handler(event, context):
+    sns = boto3.client('sns')
     order = event.get('order', event)
-
     message = f"Order processed: {json.dumps(order)}"
-
     sns.publish(
         TopicArn=os.environ['SNS_TOPIC_ARN'],
         Message=message,
         Subject='Order Processed'
     )
-
     return {
         'status': 'processed',
         'order': order
